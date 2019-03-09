49e2664a0be670e7860bb743998fb2ed3aabddc6
kdrag0n <dragon@khronodragon.com>
2019-03-08 23:30:37 -0800

    irq: Disable unhandled or broken IRQ monitoring
    This is primarily intended for generic x86 kernels which may run on all
    sorts of broken systems. Our kernel will run on a very specific hardware
    configuration with known drivers and devices, which makes this
    unnecessary.
    
    Enable noirqdebug to avoid the minor overhead added to every IRQ
    handled.
    
    Suggested-by: Tyler Nijmeh <tylernij@gmail.com>
    Change-Id: I046cd54d6fac9d40e8e9b614ef1f72c3d2ebbcb0

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -658,8 +658,8 @@ extern int irq_chip_set_type_parent(struct irq_data *data, unsigned int type);
 extern void note_interrupt(struct irq_desc *desc, irqreturn_t action_ret);
 
 
-/* Enable/disable irq debugging output: */
-extern int noirqdebug_setup(char *str);
+/* Disable irq debugging output */
+#define noirqdebug true
 
 /* Checks whether the interrupt can be requested by request_irq(): */
 extern int can_request_irq(unsigned int irq, unsigned long irqflags);
