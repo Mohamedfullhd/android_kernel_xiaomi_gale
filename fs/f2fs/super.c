6bec114a12a71a1f9a1bdc1896b755dfde8a0c0e
Christoph Hellwig <hch@lst.de>
2022-01-18 07:56:14 +0100

    UPSTREAM: unicode: clean up the Kconfig symbol confusion
    Turn the CONFIG_UNICODE symbol into a tristate that generates some always
    built in code and remove the confusing CONFIG_UNICODE_UTF8_DATA symbol.
    
    Note that a lot of the IS_ENABLED() checks could be turned from cpp
    statements into normal ifs, but this change is intended to be fairly
    mechanic, so that should be cleaned up later.
    
    Fixes: 2b3d04787012 ("unicode: Add utf8-data module")
    Reported-by: Linus Torvalds <torvalds@linux-foundation.org>
    Reviewed-by: Eric Biggers <ebiggers@google.com>
    Change-Id: I91c9031a7320e996b1cd931a18d79bfab05ee959
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Gabriel Krisman Bertazi <krisman@collabora.com>
    Signed-off-by: mrsrimar22 <mar.pashter1922@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -240,7 +240,7 @@ void f2fs_printk(struct f2fs_sb_info *sbi, const char *fmt, ...)
 	va_end(args);
 }
 
-#ifdef CONFIG_UNICODE
+#if IS_ENABLED(CONFIG_UNICODE)
 static const struct f2fs_sb_encodings {
 	__u16 magic;
 	char *name;
@@ -990,7 +990,7 @@ f2fs_info(sbi, "changing fsync mode not supported");
 		return -EINVAL;
 	}
 #endif
-#ifndef CONFIG_UNICODE
+#if !IS_ENABLED(CONFIG_UNICODE)
 	if (f2fs_sb_has_casefold(sbi)) {
 		f2fs_err(sbi,
 			"Filesystem with casefold feature cannot be mounted without CONFIG_UNICODE");
@@ -1325,7 +1325,7 @@ static void f2fs_put_super(struct super_block *sb)
 	destroy_percpu_info(sbi);
 	for (i = 0; i < NR_PAGE_TYPE; i++)
 		kvfree(sbi->write_io[i]);
-#ifdef CONFIG_UNICODE
+#if IS_ENABLED(CONFIG_UNICODE)
 	utf8_unload(sb->s_encoding);
 #endif
 	kvfree(sbi);
@@ -3422,7 +3422,7 @@ static int f2fs_scan_devices(struct f2fs_sb_info *sbi)
 
 static int f2fs_setup_casefold(struct f2fs_sb_info *sbi)
 {
-#ifdef CONFIG_UNICODE
+#if IS_ENABLED(CONFIG_UNICODE)
 	if (f2fs_sb_has_casefold(sbi) && !sbi->sb->s_encoding) {
 		const struct f2fs_sb_encodings *encoding_info;
 		struct unicode_map *encoding;
@@ -3963,7 +3963,7 @@ free_bio_info:
 	for (i = 0; i < NR_PAGE_TYPE; i++)
 		kvfree(sbi->write_io[i]);
 
-#ifdef CONFIG_UNICODE
+#if IS_ENABLED(CONFIG_UNICODE)
 	utf8_unload(sb->s_encoding);
 #endif
 free_options:
