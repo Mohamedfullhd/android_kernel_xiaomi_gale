26044db533ac12b1c428a5f0edbeaabc6ad2a14f
EndCredits <endcredits@crepuscular-aosp.icu>
2023-07-30 07:08:15 +0800

    fs: Add support for high performance booster in filesystem
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- include/linux/blk_types.h
+++ include/linux/blk_types.h
@@ -358,6 +358,7 @@ enum req_flag_bits {
 	__REQ_DRV,
 	__REQ_SWAP,		/* swapping request. */
 	__REQ_NR_BITS,		/* stops here */
+	__REQ_HPB_PREFER,	/* HPB Flag */
 };
 
 #define REQ_FAILFAST_DEV	(1ULL << __REQ_FAILFAST_DEV)
@@ -379,6 +380,8 @@ enum req_flag_bits {
 
 #define REQ_DRV			(1ULL << __REQ_DRV)
 #define REQ_SWAP		(1ULL << __REQ_SWAP)
+
+#define REQ_HPB_PREFER          (1ULL << __REQ_HPB_PREFER)
 
 #define REQ_FAILFAST_MASK \
 	(REQ_FAILFAST_DEV | REQ_FAILFAST_TRANSPORT | REQ_FAILFAST_DRIVER)
