7b883eab03ad4ea60518565387f16d003fd12e82
Mohamedfullhd <aaamohamed499@gmail.com>
2025-04-09 15:31:05 +0000

    Add display state API (for governors and etc.)

--- drivers/misc/mediatek/video/mt6768/dispsys/ddp_dsi.c
+++ drivers/misc/mediatek/video/mt6768/dispsys/ddp_dsi.c
@@ -12,6 +12,7 @@
 #include <linux/sched.h>
 #include <linux/interrupt.h>
 #include <linux/wait.h>
+#include <linux/display_state.h>
 #include "mt-plat/sync_write.h"
 #include <debug.h>
 #include "disp_drv_log.h"
@@ -200,6 +201,13 @@ static cmdqBackupSlotHandle _h_intstat;
 
 static struct LCM_mipi_read_write lcm_mipi_read_write ={0};
 static struct LCM_UTIL_FUNCS *utils = NULL;
+
+static bool display_on;
+ 
+ bool is_display_on(void)
+ {
+ 	return display_on;
+ }
 
 unsigned int impendance0[2] = { 0 }; /* MIPITX_DSI_IMPENDANCE0 */
 unsigned int impendance1[2] = { 0 }; /* MIPITX_DSI_IMPENDANCE1 */
@@ -5703,6 +5711,8 @@ int ddp_dsi_init(enum DISP_MODULE_ENUM module, void *cmdq)
 		_init_condition_wq(&(_dsi_context[i].sleep_in_done_wq));
 	}
 
+	display_on = true;
+
 	if (module == DISP_MODULE_DSIDUAL) {
 		disp_register_module_irq_callback(DISP_MODULE_DSI0,
 			_DSI_INTERNAL_IRQ_Handler);
@@ -6805,6 +6815,8 @@ int ddp_dsi_power_on(enum DISP_MODULE_ENUM module, void *cmdq_handle)
 	DSI_Reset(module, NULL);
 	_set_power_on_status(module, 1);
 
+	display_on = true;
+
 	return DSI_STATUS_OK;
 }
 
@@ -6826,6 +6838,7 @@ int ddp_dsi_power_off(enum DISP_MODULE_ENUM module, void *cmdq_handle)
 #endif
 
 	DISPFUNC();
+	display_on = false;
 	if (!_is_power_on_status(module))
 		return DSI_STATUS_OK;
 
