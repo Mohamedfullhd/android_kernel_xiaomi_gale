795c49c3117792dcb4d6accbea909c31a8196ebd
Mohamedfullhd <aaamohamed312@gmail.com>
2025-04-24 19:06:42 +0000

    Revert "fs: fix implicit declaration of function 'ksu_handle_faccessat'"
    This reverts commit f1602bad0b11378dc1fc6514209a0505a93db1e4.

--- fs/open.c
+++ fs/open.c
@@ -353,11 +353,6 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
  * We do this by temporarily clearing all FS-related capabilities and
  * switching the fsuid/fsgid around to the real ones.
  */
-#ifdef CONFIG_KSU
-extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
-			 int *flags);
-#endif
-
 long do_faccessat(int dfd, const char __user *filename, int mode)
 {
 	const struct cred *old_cred;
@@ -459,6 +454,10 @@ out:
 	return res;
 }
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
+			 int *flags);
+#endif
 
 SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 {
