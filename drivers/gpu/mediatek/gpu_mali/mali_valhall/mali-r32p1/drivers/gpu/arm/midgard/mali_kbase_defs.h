32a1b30c4160e0d669c64eafa21b2a7014009ec5
Mohamedfullhd <aaamohamed312@gmail.com>
2025-04-25 20:48:35 +0000

    mali_kbase: platform: Add per-process and global sysfs nodes for GPU mem usage

--- drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mali_kbase_defs.h
+++ drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mali_kbase_defs.h
@@ -639,6 +639,8 @@ struct kbase_devfreq_queue_info {
  *                      Used to ensure that pages of allocation are accounted
  *                      only once for the process, even if the allocation gets
  *                      imported multiple times for the process.
+ * @kobj:               Links to the per-process sysfs node
+ *                      &kbase_device.proc_sysfs_node.
  */
 struct kbase_process {
 	pid_t tgid;
@@ -648,6 +650,7 @@ struct kbase_process {
 
 	struct rb_node kprcs_node;
 	struct rb_root dma_buf_root;
+	struct kobject kobj;
 };
 
 /**
@@ -945,6 +948,7 @@ struct kbase_process {
  *                          @total_gpu_pages from imported dma buffers.
  * @gpu_mem_usage_lock:     This spinlock should be held while accounting
  *                          @total_gpu_pages for both native and dma-buf imported
+ * @proc_sysfs_node:        Sysfs directory node to store per-process stats.
  *                          allocations.
  * @dummy_job_wa:           struct for dummy job execution workaround for the
  *                          GPU hang issue
@@ -1220,6 +1224,7 @@ struct kbase_device {
 	struct job_status_qos job_status_addr;
 	struct v1_data* v1;
 #endif
+	struct kobject *proc_sysfs_node;
 };
 
 /**
